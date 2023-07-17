c77d0af75246cbae0df6c88cf6e98e5d269c028f
seon-mikim <tjsalsun@gmail.com>
2023-07-17 12:00:36 +0900

    conf: 초기 세팅

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header"
+import Keyboard from "./Keyboard"
+import Wordboard from "./Wordboard"
+
+
+function Layout() {
+	return (
+		<div>
+			<Header />
+			<Wordboard />
+			<Keyboard />
+		</div>
+	)
+}
+
+export default Layout
