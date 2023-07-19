513e46977f11890ec02b5a54eab444c8bd47dd2f
seon-mikim <tjsalsun@gmail.com>
2023-07-20 03:03:35 +0900

    feat: provider 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import WordProvider from './context/WordContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WordProvider>
+      <App />
+    </WordProvider>
+  </React.StrictMode>
+);
