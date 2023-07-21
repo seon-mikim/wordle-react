5b7cb9f05da4fb165fa281e1e631e3256e85e55b
seon-mikim <tjsalsun@gmail.com>
2023-07-21 21:52:49 +0900

    feat: 키보드 상수데이터 생성

---
+++ src/constants/KeyboardData.js
@@ -0,0 +1,7 @@
+
+const firstKey = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I','O','P'];
+const secondKey = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+const thirdKey = ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'x'];
+export const KeyboardData = [firstKey, secondKey, thirdKey]
+
+console.log(KeyboardData)
